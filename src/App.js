4fce81f6ac94e3a99bff08877fe4de2dedc4ecc5
Ant0ineB <bardet.antoine64230@gmail.com>
2026-01-14 17:38:33 +0100

    Adding modal window

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 
 class App extends React.Component {
+
+  
+  
+ 
+
+export default function App(){
+  const[users,setUsers] = useState([]);
+  
+  const getUsers = async () => {
+  const res = await fetch("https://api.github.com/users/github-john-doe");
+  const json = await res.json();
+  setUsers(json);
+  }
+  useEffect(() => {
+    getUsers();
+  },[])
+
   render() {
     return(
     )
   }
+};
 }
-export default App;
